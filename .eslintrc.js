e50637993132d38e45b95043c5decd3ee3d1e2d7
Sam De Maeyer <sam_demaeyer@hotmail.com>
2021-02-07 12:46:17 +0000

    Setup project part 2

---
+++ .eslintrc.js
@@ -0,0 +1,28 @@
+module.exports = {
+  env: {
+    browser: true,
+    es2020: true,
+  },
+  extends: [
+    'plugin:react/recommended',
+    'plugin:@typescript-eslint/recommended',
+    'prettier/@typescript-eslint',
+    'plugin:prettier/recommended',
+  ],
+  parser: '@typescript-eslint/parser',
+  parserOptions: {
+    ecmaFeatures: {
+      jsx: true,
+    },
+    ecmaVersion: 11,
+  },
+  plugins: ['react', '@typescript-eslint'],
+  rules: {
+    semi: ['error', 'never'],
+  },
+  settings: {
+    react: {
+      version: 'detect',
+    },
+  },
+}
